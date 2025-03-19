f09a4761fc2a1d27ab192faed5f8b81ac1a39508
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-03-19 18:57:27 -0300

    Implementação e estilização do Post

---
+++ modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs/src/components/Post.jsx
@@ -0,0 +1,33 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/diego3g.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Diego Fernandes</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='20 de Março às 18:05h' datetime="2025-03-20 18:05:45">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉<a href=""> jane.design/doctorcare</a></p>
+
+                <p> 
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '} 
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
