ac33d05a372af74f4f0d27c0933dc3b2d980d12d
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-03-20 07:30:28 -0300

    Implementado e estilizado o campo de Comentários e botã Publicar

--- modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs/src/components/Post.jsx
+++ modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs/src/components/Post.jsx
@@ -17,9 +17,7 @@ export function Post() {
 
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
-
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
                 <p>👉<a href=""> jane.design/doctorcare</a></p>
 
                 <p> 
@@ -28,6 +26,18 @@ export function Post() {
                     <a href="">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                placeholder='Deixe um comentário'
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
