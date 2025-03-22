a2031b92f463ae71138b6ead0a76139171250e1d
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-03-22 11:11:07 -0300

    Implementado os Posts e o incremento de comentários

--- modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs/src/App.jsx
+++ modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs/src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content:'Fala galeraa 👋'},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content:'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-20 8:50:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content:'Fala galeraa 👋'},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content:'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-20 13:25:00')
+  },
+];
 
 export function App() {
    return (
@@ -15,14 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Ricardo Andrade" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora perspiciatis tempore quisquam recusandae vitae, nihil eos possimus consequuntur? "
-          />
-          <Post 
-            author="Ricardo Andrade" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora perspiciatis tempore quisquam recusandae vitae, nihil eos possimus consequuntur? Ducimus vitae molestias amet architecto impedit iure obcaecati voluptatibus quos eaque maiores!"
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
