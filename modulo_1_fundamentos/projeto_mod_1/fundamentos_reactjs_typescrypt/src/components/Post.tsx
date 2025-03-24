4fe83200c51b6dac6c2420ef8459ce79d56831bb
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-03-24 16:07:16 -0300

    Adequação do projeto para TypeScript

--- modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs_typescrypt/src/components/Post.tsx
+++ modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs_typescrypt/src/components/Post.tsx
@@ -1,13 +1,30 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR';
+import { FormEvent, useState, ChangeEvent, InvalidEvent } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
-import { useState } from 'react';
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string,
+}
+
+interface PostProps {
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
     const [comments, setComments] = useState([
         'Post muito bacana!, hein?!'
     ])
@@ -24,19 +41,23 @@ export function Post({ author, publishedAt, content }) {
     })
 
 
-    function handleCreateNewComment() {
+    function handleCreateNewComment(event:FormEvent) {
         event.preventDefault()
 
         setComments([...comments, newCommentText]);
         setNewCommentText('');
     }
 
-    function handleNewCommentChange() {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
     }
 
-    function deleteComment(commentToDelete) {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('Esse campo é obrigatório')
+    }
+
+    function deleteComment(commentToDelete: string) {
         const commentsWithoutDeletedOne = comments.filter(comment => {
             return comment !== commentToDelete;
         });
@@ -44,9 +65,7 @@ export function Post({ author, publishedAt, content }) {
         setComments(commentsWithoutDeletedOne);
     }
 
-    function handleNewCommentInvalid() {
-        event.target.setCustomValidity('Esse campo é obrigatório')
-    }
+
 
     const isNewCommentEmpty = newCommentText.length == 0
 
