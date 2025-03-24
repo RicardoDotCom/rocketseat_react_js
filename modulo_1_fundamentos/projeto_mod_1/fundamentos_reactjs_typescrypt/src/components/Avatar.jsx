1830534637423b0eb622557a234e8d2ad3c43dc2
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-03-24 15:30:37 -0300

    Inicio do projeto em TS

--- modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs_typescrypt/src/components/Avatar.jsx
+++ modulo_1_fundamentos/projeto_mod_1/fundamentos_reactjs_typescrypt/src/components/Avatar.jsx
@@ -4,7 +4,7 @@ export function Avatar({ hasBorder = true, src }) {
     return (
         <img 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-            src={src} 
+            src={src}
         />
     );
 }
