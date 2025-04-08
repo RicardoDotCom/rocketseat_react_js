01ebda1229cf2bfdf1162709028e56e982585318
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2025-04-08 08:27:30 -0300

    Finalização do Projeto Pomodoro

--- modulo_2_hooks/projeto_mod_2/hooks_pomodoro/src/pages/Home/Components/NewCycleForm/index.tsx
+++ modulo_2_hooks/projeto_mod_2/hooks_pomodoro/src/pages/Home/Components/NewCycleForm/index.tsx
@@ -19,9 +19,9 @@ export function NewCycleForm() {
             />
 
             <datalist id="task-suggestions">
-               <option value="Pojeto 1" />
-               <option value="Pojeto 2" />
-               <option value="Pojeto 3" />
+               <option value="Projeto 1" />
+               <option value="Projeto 2" />
+               <option value="Projeto 3" />
                <option value="PlayStation" />
             </datalist>
 
